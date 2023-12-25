a8450edd30551975c26019b1d8fc799ef28f7d36
Betula <1457922195@qq.com>
2023-12-25 08:08:39 +0000

    fix get /pdf/id

--- services/backend/src/crud/pdf_data.py
+++ services/backend/src/crud/pdf_data.py
@@ -5,6 +5,7 @@ from src.database.models import PdfData
 from src.schemas.pdf_data import PdfOutSchema
 from src.schemas.token import Status
 
-async def get_by_id(pdf_id) -> PdfOutSchema:
-    return await PdfOutSchema.from_queryset_single(PdfData.get(id=pdf_id))
 
+async def get_by_id(pdf_id) -> PdfOutSchema:
+    return await PdfOutSchema.from_queryset_single(PdfData.get(paper_id=pdf_id)
+                                                   )
--- services/backend/src/routes/pdf_data.py
+++ services/backend/src/routes/pdf_data.py
@@ -8,15 +8,13 @@ from src.schemas.token import Status
 
 router = APIRouter()
 
+
 @router.get(
     "/pdf/id",
     response_model=PdfOutSchema,
 )
-async def get_pdf(pdf_id: int) -> PdfOutSchema:
+async def get_pdf(pdf_id: str) -> PdfOutSchema:
     try:
         return await crud.get_by_id(pdf_id)
     except DoesNotExist:
-        raise HTTPException(
-            status_code=404,
-            detail="Pdf does not exist"
-        )
+        raise HTTPException(status_code=404, detail="Pdf does not exist")
